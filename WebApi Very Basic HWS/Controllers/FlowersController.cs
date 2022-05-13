b0a6f998d986c54e9e4b5d97ea3ebb6e58aa6036
אוהד סעדיה <ohad24333@gmail.com>
2022-05-14 02:44:48 +0300

    Add project files.

---
+++ WebApi Very Basic HWS/Controllers/FlowersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_Very_Basic_HWS.Controllers
+{
+    [Route("api/[action]")]
+    [ApiController]
+    public class FlowersController : ControllerBase
+    {
+        public string Rose()
+        {
+            return "Rose";
+        }
+        public string Lily()
+        {
+            return "Lily";
+        }
+        public string Tulip()
+        {
+            return "Tulip";
+        }
+    }
+}
